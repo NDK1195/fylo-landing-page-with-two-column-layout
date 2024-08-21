894a8d1f8f013eb892e7cd2c9732dd3ad8314a51
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-08-21 11:22:57 +0700

    add project structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# Frontend Mentor - Fylo landing page with two column layout solution
+
+This is a solution to the [Fylo landing page with two column layout challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/fylo-landing-page-with-two-column-layout-5ca5ef041e82137ec91a50f5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+### Screenshot
+
+![](./images/Screenshot.png)
+
+### Links
+
+- Solution URL: [Solution URL here](https://github.com/NDK1195/huddle-landing-page-with-single-introductory-section)
+- Live Site URL: [Live site URL here](https://ndk1195.github.io/huddle-landing-page-with-single-introductory-section/)
+
+### Built with
+
+- HTML5
+- Tailwind CSS
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "open-sans": ["Open Sans", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+      colors: {
+        "very-dark-blue": "hsl(243, 87%, 12%)",
+        "desaturated-blue": "hsl(238, 22%, 44%)",
+        "bright-blue": "hsl(224, 93%, 58%)",
+        "moderate-cyan": "hsl(170, 45%, 43%)",
+        "light-grayish-blue": "hsl(240, 75%, 98%)",
+        "light-gray": "hsl(0, 0%, 75%)",
+      },
+      boxShadow: {
+        "box-mobile": "0px 0px 8px 4px rgba(56, 56, 56, 0.05)",
+        "box-desktop": "0px 0px 10px 5px rgba(56, 56, 56, 0.05)",
+      },
+      dropShadow: {
+        button: "2px 3px 6px rgba(0, 0, 0, 0.16)",
+      },
+    },
+  },
+  plugins: [],
+};
